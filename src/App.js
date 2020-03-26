01c4563661d3a47316abd800fff3df1fca3e8a5c
axe i-grec <tag4rce7@gmail.com>
2020-03-26 08:46:26 +0100

    First HTML / CSS draft

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return ( 
+      <div className="App">
+        <div id="editor-container">
+          <div id="editor-header" className="box-header"><p>EDITOR</p></div>
+          <textarea id="editor" cols="30" rows="10"></textarea>
+        </div>
+        <div id="preview-container">
+          <div id="preview-header" className="box-header"><p>PREVIEW</p></div>
+          <div id="preview">
+            <h1>Ah yeah</h1>
+            <p>Hello</p>
+          </div>
+        </div>
+      </div>
+     );
+  }
 }
-
+ 
 export default App;
