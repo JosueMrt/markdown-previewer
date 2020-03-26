b6f8b26a3795ee41ea51f3cad8318d77461bdcd9
axe i-grec <tag4rce7@gmail.com>
2020-03-26 10:02:33 +0100

    fixed test #4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import marked from 'marked';
+import welcomeMsg from './welcomeMsg';
 
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      input: ``
+      input: welcomeMsg
     }
     this.handleChange = this.handleChange.bind(this)
   }
@@ -21,13 +22,11 @@ class App extends React.Component {
       <div className="App">
         <div id="editor-container">
           <div id="editor-header" className="box-header"><p>EDITOR</p></div>
-          <textarea id="editor" cols="30" rows="10" onChange={this.handleChange}></textarea>
+          <textarea id="editor" cols="30" rows="10" value={this.state.input} onChange={this.handleChange}></textarea>
         </div>
         <div id="preview-container">
           <div id="preview-header" className="box-header"><p>PREVIEW</p></div>
-          <div id="preview">
-            <div dangerouslySetInnerHTML={{__html:marked(this.state.input)}}></div>
-          </div>
+          <div id="preview" dangerouslySetInnerHTML={{__html:marked(this.state.input)}}></div>
         </div>
       </div>
      );
---
+++ src/welcomeMsg.js
@@ -0,0 +1,5 @@
+const welcomeMsg = `# HEADING 1
+ok cool
+<b>bold stuff</b>`
+
+export default welcomeMsg
