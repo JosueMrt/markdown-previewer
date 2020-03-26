f3627489d81e76517b4c0fcc91d09e977dbd40de
axe i-grec <tag4rce7@gmail.com>
2020-03-26 09:45:01 +0100

    Installed marked.js and roughly implemented it

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import './App.css';
+import marked from 'marked';
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = {
+      input: ``
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    })
   }
   render() { 
     return ( 
       <div className="App">
         <div id="editor-container">
           <div id="editor-header" className="box-header"><p>EDITOR</p></div>
-          <textarea id="editor" cols="30" rows="10"></textarea>
+          <textarea id="editor" cols="30" rows="10" onChange={this.handleChange}></textarea>
         </div>
         <div id="preview-container">
           <div id="preview-header" className="box-header"><p>PREVIEW</p></div>
           <div id="preview">
-            <h1>Ah yeah</h1>
-            <p>Hello</p>
+            <div dangerouslySetInnerHTML={{__html:marked(this.state.input)}}></div>
           </div>
         </div>
       </div>
