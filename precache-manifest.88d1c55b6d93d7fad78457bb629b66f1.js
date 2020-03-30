9a37afcaf8d24b3d55639a1fbb0a5f47c0c1748d
axe i-grec <tag4rce7@gmail.com>
2020-03-30 08:13:55 +0200

    Updates

---
+++ precache-manifest.88d1c55b6d93d7fad78457bb629b66f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ff4f4fea56e7fcc0bbee56e3716a3d",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "a322906ccb157b751a2f",
+    "url": "/markdown-previewer/static/css/main.453b5d0b.chunk.css"
+  },
+  {
+    "revision": "235503b2b051d8204774",
+    "url": "/markdown-previewer/static/js/2.c8fb0c9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/markdown-previewer/static/js/2.c8fb0c9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a322906ccb157b751a2f",
+    "url": "/markdown-previewer/static/js/main.fb14715c.chunk.js"
+  },
+  {
+    "revision": "c17240ee52ec60d7235d",
+    "url": "/markdown-previewer/static/js/runtime-main.55587a8c.js"
+  }
+]);
